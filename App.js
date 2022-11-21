d4b0f635d8a1f6803c1bbe9f4fe48c128ab83d1f
Kaija Collette <87038342+caperatel7@users.noreply.github.com>
2022-11-21 16:03:28 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Morty from './Morty';
+
+function App() {
+  return (
+    <div className="App">
+      <Morty />
+  
+    </div>
+  );
+}
+
+export default App;
